2be47038bc62c8b6fc0785d4bd087af59cb75fa6
bb <bezalelcohen1@gmail.com>
2021-02-23 19:42:09 +0200

    a]

--- structure/listnode.py
+++ structure/listnode.py
@@ -9,31 +9,26 @@ class Node:
 
 
 class List:
-    def __init__(self, head=None, last=None, n=0, other=None):
+    def __init__(self, head=None, last=None, other=None):
         self.head = head
         self.last = last
         self.n = 0
 
     @property
     def head(self):
-        return self.head
+        return self.__head
 
     @head.setter
-    def head(self, node):
-        self.head = node
+    def head(self, head):
+        self.__head = head
 
-    # @head.getter
-    # def head(self):
-    #     return
+    @property
+    def last(self):
+        return self.__last
 
-    # @property
-
-    # def last(self):
-    #     return self.last
-    #
-    # @property
-    # def n(self):
-    #     return self.n
+    @last.setter
+    def last(self, last):
+        self.__last = last
 
     def __str__(self) -> str:
         p = self.head
@@ -41,32 +36,39 @@ class List:
         while p is not None:
             s += p.__str__() + "\n"
             p = p.next
-        s += "-------------------------------------------------------\n"
+        s += "---------------------------  n = {}  -----------------\n".format(self.n)
         return s
 
     def __add__(self, other, add_to_end=False):
         self.n += 1
         if self.head is None:
             self.head = self.last = other
-            return List()
-        # if add_to_end:
-        #
-        # self.head.next = other
+        if add_to_end:
+            other.prev = self.last
+            self.last.next = other
+            self.last = other
+        else:
+            other.next = self.head
+            self.head.prev = other
+            self.head = other
+        return self
 
     @staticmethod
     def create_list(array):
-        lis = List()
-        p = Node(array[0])
-        lis.head = p
-        for item in array[1:]:
-            p.next = Node(item, prev=p)
-            p = p.next
-        return lis
+        if len(array) == 0:
+            return
+        l = List()
+        for item in array:
+            l += Node(item)
+            # p.next = Node(item, prev=p)
+            # p = p.next
+        return l
 
 
 if __name__ == '__main__':
     a = [1, 3, 4]
-    # lis = List.create_list(a)
+    l = List(7)
+    lis = List.create_list(a)
     n1 = Node(4)
-    # lis += n1
-    print(n1)
+    lis += n1
+    print(lis)
--- temp.py
+++ temp.py
@@ -2,6 +2,25 @@ import dynamic
 import graph
 from structure.listnode import *
 
-s = "j"
-s += "p"
-print(s)
+
+class P:
+
+    def __init__(self, x):
+        self.x = x
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if x < 0:
+            self.__x = 0
+        elif x > 1000:
+            self.__x = 1000
+        else:
+            self.__x = x
+
+
+p = P(-3)
+print(p.x)
