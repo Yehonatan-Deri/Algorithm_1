9905ccce1140cd6391946c75431eed9ed1bc702b
bb <bezalelcohen1@gmail.com>
2021-02-26 17:55:37 +0200

    adding: topology() , search_circle()

--- graph/temp.py
+++ graph/temp.py
@@ -1,9 +1,13 @@
 from Algorithm_1.structure.graph_struct import Graph, GraphNotAimed, Edge, Vertice
 import copy
+import numpy as np
+from typing import Union, Tuple, List
 
 
-# efficiency: O(V+E)
-def dfs(G: Graph) -> tuple:  # -> tuple(dict, dict, dict)
+def dfs(G: Graph) -> Tuple[dict, dict, dict]:
+    """
+    efficiency: O(V+E)
+    """
     time = 0
     pi, color, d, f = {v: None for v in G.V}, {v: 'white' for v in G.V}, {}, {}
 
@@ -20,11 +24,42 @@ def dfs(G: Graph) -> tuple:  # -> tuple(dict, dict, dict)
     for v in G.V:
         if color[v] == 'white':
             dfs_visit(v)
-    return pi, d, f
+    return pi, d, f,
 
 
-# efficiency: O(V+E)
-def bfs(G: Graph):
+def search_circle(G: Graph) -> Union[bool, dict]:  # -> tuple(dict, dict, dict)
+    """
+    search circle with dfs
+    return: dict of exit time if circle not found or False if circle found
+    efficiency: O(V+E)
+    """
+    time = 0
+    color, f = {v: 'white' for v in G.V}, {}
+
+    def dfs_visit(v: Vertice):
+        nonlocal time
+        color[v], time = 'gray', time + 1
+        for u in v.Adj:
+            if color[u] == 'white':
+                dfs_visit(u)
+            elif color == 'white':
+                return False
+
+            color[v] = 'black'
+
+        time, f[v] = time + 1, time + 1
+
+    for v in G.V:
+        if color[v] == 'white':
+            if not dfs_visit(v):
+                return False
+    return f
+
+
+def bfs(G: Graph) -> Tuple[dict, dict]:
+    """
+    efficiency: O(V+E)
+    """
     pi, color, layer, Q = {v: None for v in G.V}, {v: 'white' for v in G.V}, {G.V[0]: 1}, [G.V[0]]
 
     while len(Q):
@@ -39,9 +74,11 @@ def bfs(G: Graph):
 
 
 # efficiency: O(V+E)
-def topology(G: Graph):
-    pi, d, f = dfs(G)
-    f.sort()
+def topology(G: Graph) -> list:
+    _, _, f = dfs(G)
+    topology_order = list(f.items())
+    topology_order.sort(key=lambda item: item[1], reverse=True)
+    return [item[0] for item in topology_order]
 
 
 # strongly connected component graph
@@ -60,4 +97,20 @@ if __name__ == '__main__':
     G.connect(from_=V[1], to=V[2], weight=1)
     G.connect(from_=V[2], to=V[1], weight=1)
     G.connect(e=r)
-    print(bfs(G))
+
+    # topology(G)
+    print(topology(G))
+
+    # print(c)
+    #
+    # Vector = List[float]
+    # print(Vector)
+    #
+    #
+    # def scale(scalar: float, vector: Vector) -> Vector:
+    #     return [scalar * num for num in vector]
+    #
+    #
+    # # typechecks; a list of floats qualifies as a Vector.
+    # new_vector = scale(2.0, [1.0, -4.2, 5.4])
+    # print(new_vector)
--- temp.py
+++ temp.py
@@ -1,26 +1,4 @@
-import dynamic
-import graph
-from structure.list_struct import *
+from typing import NewType
 
-
-class P:
-
-    def __init__(self, x):
-        self.x = x
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, x):
-        if x < 0:
-            self.__x = 0
-        elif x > 1000:
-            self.__x = 1000
-        else:
-            self.__x = x
-
-
-p = P(-3)
-print(p.x)
+UserId = NewType('UserId', int)
+some_id = UserId(524313)
